511791d6ce920028e0f44233b6c4a0ea3efac046
KRISHNADAS-KD <mrkrishnadaspv@gmail.com>
2024-09-07 01:24:08 +0400

    Update Boost Screen Controller,BL,DL ,DB pending

---
+++ DLLayer/DLBoost.cs
@@ -0,0 +1,87 @@
+﻿using DllLayer.PgSqlHelper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLayer
+{
+    public class DLBoost
+    {
+        public object? SaveBoostIfNotExists(DataTable referralData, IDbConnection connection)
+        {
+            try
+            {
+                NpgsqlParameter[] parameters = PgsqlHelper.GetSpParameterSet(connection, "save_boost_if_not_exists");
+
+                MapReferralDataToParameters(referralData, parameters);
+                DataSet result = PgsqlHelper.ExecuteFunctionWithTransaction("save_boost_if_not_exists", parameters, connection, "result");
+                return result.Tables["result"] ?? null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object? UpdateBoostData(DataTable referralData, IDbConnection connection)
+        {
+            try
+            {
+                NpgsqlParameter[] parameters = PgsqlHelper.GetSpParameterSet(connection, "update_boost");
+                DataSet result = PgsqlHelper.ExecuteFunctionWithTransaction("update_boost", parameters, connection, "result");
+                return result.Tables["result"] ?? null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object? FetchBoostData(DataTable referralData, IDbConnection connection)
+        {
+            try
+            {
+                NpgsqlParameter[] parameters = PgsqlHelper.GetSpParameterSet(connection, "fetch_boost");
+                DataSet result = PgsqlHelper.ExecuteFunctionWithTransaction("fetch_boost", parameters, connection, "result");
+                return result.Tables["result"] ?? null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        //chnage paramter mapping
+        private void MapReferralDataToParameters(DataTable data, NpgsqlParameter[] parameters)
+        {
+            // Iterate through each row in the DataTable
+            foreach (DataRow row in data.Rows)
+            {
+                foreach (var parameter in parameters)
+                {
+                    switch (parameter.ParameterName)
+                    {
+                        case "p_referrer_id":
+                            parameter.Value = row["userid"] != DBNull.Value ? row["userid"].ToString() : string.Empty;
+                            break;
+                        case "p_username":
+                            parameter.Value = row["username"].ToString();
+                            break;
+                        case "p_referred_id":
+                            parameter.Value = row["referrerid"] != DBNull.Value ? row["referrerid"].ToString() : string.Empty;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ H2O/Controllers/BoostController.cs
@@ -0,0 +1,49 @@
+﻿
+using BLLayer;
+using H2O.Models;
+using Hydrogen.Common;
+using Microsoft.AspNetCore.Mvc;
+using static H2O.Enumerators.CrudTypes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace H2O.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoostController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(Criteria Data)
+        {
+            object? Result = null;
+            BLBoost objBLBoost = new BLBoost();
+            switch (Data.CrudType)
+            {
+                case CrudType.Create:
+                    if (Data.SaveData.Tables.Contains("collect"))
+                    {
+                        Result = objBLBoost.Create(Data.SaveData.Tables["collect"]);
+                    }
+                    else
+                    {
+                        Result = MessageLib.Error;
+                    }
+                    break;
+                case CrudType.Read:
+                    if (Data.Mode == 0)
+                    {
+                        Result = objBLBoost.FetchCoin(Data.FetchData);
+                    }
+                    else if (Data.Mode == 1)
+                    {
+                        Result = objBLBoost.Update(Data.FetchData);
+                    }
+                    break;
+                default:
+                    // Handle other CRUD operations if needed
+                    break;
+            }
+            return Ok(Result);
+        }
+    }
+}
